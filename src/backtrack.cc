5791400f39259ef91e6f43a820d5605799241309
Dongmin Lee <ldm2468@gmail.com>
2021-05-22 19:52:45 +0900

    Implement embedding verification

--- src/backtrack.cc
+++ src/backtrack.cc
@@ -8,6 +8,30 @@
 Backtrack::Backtrack() {}
 Backtrack::~Backtrack() {}
 
+bool Backtrack::Verify(const Graph &data, const Graph &query,
+                       const std::vector<Vertex> &embedding) {
+#ifdef VERIFY_EMBEDDINGS
+  size_t vertexCount = query.GetNumVertices();
+  if (vertexCount != embedding.size()) {
+    // even the size doesn't match
+    return false;
+  }
+  // check every possible edge of the embedding
+  for (Vertex i = 0; i < vertexCount; i++) {
+    for (Vertex j = i + 1; j < vertexCount; j++) {
+      bool queryIsNeighbor = query.IsNeighbor(i, j);
+      bool embeddingIsNeighbor = data.IsNeighbor(embedding[i], embedding[j]);
+      if (queryIsNeighbor != embeddingIsNeighbor) {
+        return false;
+      }
+    }
+  }
+  return true;
+#else
+  return true;
+#endif
+}
+
 void Backtrack::PrintAllMatches(const Graph &data, const Graph &query,
                                 const CandidateSet &cs) {
   std::cout << "t " << query.GetNumVertices() << "\n";
